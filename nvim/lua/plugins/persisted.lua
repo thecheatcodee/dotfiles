57c47e6e7147bc3d3e966c4a1465dc1f9c20a01f
Zach Lü <zachlu@ZachdeMacBook-Air.local>
2024-11-13 21:21:15 +0800

    使用persisted.nvim管理会话，并修复原来会话管理的一些bug

--- nvim/lua/plugins/persisted.lua
+++ nvim/lua/plugins/persisted.lua
@@ -1,37 +1,57 @@
 ---@type NvPluginSpec
 return {
   "olimorris/persisted.nvim",
-  enabled = false,
+  enabled = true,
   lazy = false, -- make sure the plugin is always loaded at startup
-  -- keys = {},
+  opts = {
+    autoload = true,
+    autosave = true,
+    use_git_branch = true,
+    allowed_dirs = {}, -- Table of dirs that the plugin will start and autoload from
+    ignored_dirs = {
+      -- "~" -- home dir
+    }, -- Table of dirs that are ignored for starting and autoloading
+    telescope = {
+    mappings = { -- Mappings for managing sessions in Telescope
+      copy_session = "<C-c>",
+      change_branch = "<C-b>",
+      delete_session = "<C-d>",
+    },
+    -- FIXME: 没用
+    icons = { -- icons displayed in the Telescope picker
+      selected = " ",
+      dir = "  ",
+      branch = " ",
+    },
+    should_save = function()
+      -- Do not save if the alpha dashboard is the current filetype
+      if vim.bo.filetype == "nvdash" then
+        return false
+      end
+      return true
+    end,
+
+    -- on_autoload_no_session = function()
+    --   vim.notify("No existing session to load.")
+    -- end
+
+  },
+
+  },
+  config = function(_, opts)
+    local persisted = require("persisted")
+    -- 没用
+    -- require("telescope").load_extension("persisted")
+    persisted.branch = function()
+      local branch = vim.fn.systemlist("git branch --show-current")[1]
+      return vim.v.shell_error == 0 and branch or nil
+    end
+    persisted.setup(opts)
+  end,
   keys = {
-    {
-      "<leader>qs",
-      function()
-        require("persisted").load()
-      end,
-      desc = "Restore Session",
-    },
-    {
-      "<leader>qS",
-      function()
-        require("persisted").select()
-      end,
-      desc = "Select Session",
-    },
-    {
-      "<leader>ql",
-      function()
-        require("persisted").load { last = true }
-      end,
-      desc = "Restore Last Session",
-    },
-    {
-      "<leader>qd",
-      function()
-        require("persisted").stop()
-      end,
-      desc = "Don't Save Current Session",
-    },
+    { "<leader>qs", "<cmd>SessionLoad<cr>", desc = "Restore Session", },
+    { "<leader>qS", "<cmd>SessionSelect<cr>", desc = "Select Session", },
+    { "<leader>ql", "<cmd>SessionLoadLast<cr>", desc = "Restore Last Session", },
+    { "<leader>qd", "<cmd>SessionDelete<cr>", desc = "Don't Save Current Session", },
   },
 }
